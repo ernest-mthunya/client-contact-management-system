b024bb38271bc5fd079748beffb4a66535ff6538
Enerst Mthunya <ernestmthunya@gmail.com>
2026-03-11 11:37:10 +0200

    added default mvc project

---
+++ client-contact-management/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using client_contact_management.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace client_contact_management.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ client-contact-management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace client_contact_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
