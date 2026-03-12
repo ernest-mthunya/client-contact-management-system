6c0d3dac31189ef5a0bb27a76d1bf3b7bf8439ea
Enerst Mthunya <ernestmthunya@gmail.com>
2026-03-12 09:17:07 +0200

    added query indexes

--- client-contact-management/Data/ClientContactManagementDbContext.cs
+++ client-contact-management/Data/ClientContactManagementDbContext.cs
@@ -36,6 +36,28 @@ namespace client_contact_management.Data
             modelBuilder.Entity<Contact>()
                 .HasIndex(c => c.Email)
                 .IsUnique();
+
+            modelBuilder.Entity<Client>()
+                .HasIndex(c => c.ClientCode)
+                .IsUnique()
+                .HasDatabaseName("IX_Clients_ClientCode");
+
+            modelBuilder.Entity<Client>()
+                .HasIndex(c => c.Name)
+                .HasDatabaseName("IX_Clients_Name");
+
+            modelBuilder.Entity<Contact>()
+                .HasIndex(c => c.Email)
+                .IsUnique()
+                .HasDatabaseName("IX_Contacts_Email");
+
+            modelBuilder.Entity<Contact>()
+                .HasIndex(c => new { c.Surname, c.Name })
+                .HasDatabaseName("IX_Contacts_Surname_Name");
+
+            modelBuilder.Entity<ClientContact>()
+                .HasIndex(cc => new { cc.ContactId, cc.ClientId })
+                .HasDatabaseName("IX_ClientContacts_ContactId_ClientId");
         }
     }
 }
